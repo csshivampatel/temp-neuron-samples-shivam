5b9b45b8fcb71b0be8b2a396900f1c15e06aab29
csshivampatel <shivam.patel@confidosoft.in>
2024-04-16 18:30:41 +0530

    initial commit of the samples

---
+++ Services/RESTServices/ClientOAuth/MvcClientSample/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcClient.Models;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            if(!User.Identity.IsAuthenticated) 
+            {
+                return RedirectToAction(nameof(AccountController.Login), "Account");
+            }
+
+            
+            return View();
+        }
+
+        public IActionResult LoggedOut()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Login()
+        {
+           // var authResult = HttpContext.AuthenticateAsync("Cookies").Result;
+            return View("Index");
+        }
+
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "oidc");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
